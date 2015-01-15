846f736abed307f5842187a58a734ebdbacc4c12
carlos <carlos@carlosw.(none)>
2015-01-15 20:18:17 -0200

    commit Site PDO

---
+++ site_pdo_ce/lib/PDOConnectionFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+class PDOConnectionFactory {
+    public static function getConnection() {
+        try {
+            
+          
+            $config = include '/var/www/site_pdo_ce/config/config.php';
+
+            if (!isset($config['db']['host'])) {
+                throw new InvalidArgumentException("Conexao nao existe");
+            }
+
+            $host = isset($config['db']['host']) ? ($config['db']['host']) : null;
+            $dbname = isset($config['db']['dbname']) ? ($config['db']['dbname']) : null;
+            $user = isset($config['db']['user']) ? ($config['db']['user']) : null;
+            $password = isset($config['db']['password']) ? ($config['db']['password']) : null;
+
+
+            return $pdo = new PDO("mysql:host=" . $host . ";dbname=" . $dbname, $user, $password);
+        } catch (Exception $exc) {
+            echo $exc->getTraceAsString();
+        }
+    }
+}
